f9be57a3e2bf503d4fadbd027c2dbcf95fa43605
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2023-02-06 09:52:03 +0000

    basics of async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """
+    Wait for a random delay of number between 0 and 10
+    using asynchronous coroutine
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
