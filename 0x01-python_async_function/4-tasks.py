79ae4c13fa8baf7a5fa6bf7a12f5503a69b5f6ed
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2023-02-06 11:28:11 +0000

    task 4 - datatype fixed

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,7 +8,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n, max_delay):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Execute multiple coroutines at the same time with async
     """
